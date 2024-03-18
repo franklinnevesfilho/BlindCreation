45e0f7f1a30cb91ef41c41e55618f7402507706f
frank <franklin.neves.filho@gmail.com>
2024-03-18 16:08:27 -0400

    updated bottomrail model

--- src/components/service/BottomRailService.tsx
+++ src/components/service/BottomRailService.tsx
@@ -1,58 +1,65 @@
 import { BottomRail } from '../models/BottomRail';
 
 class BottomRailService {
-    bottomRails: BottomRail[] = [
+    private static instance: BottomRailService;
+
+    private bottomRails: BottomRail[] = [
         {
             id: "0-151-AL-ESW19",
             name: "Rollux-AL. Bottomrail Euro Slim White 19'",
-            weight: 0.2822,
-            weightUnit: "lb/ft"
+            weight:{
+                value: 0.2822,
+                unit: "lb/ft"
+            }
         },
         {
             id: "0-151-AL-ALW16",
             name: "Rollux-AL. Bottomrail SLIM White 16'",
-            weight: 0.2001,
-            weightUnit: "lb/ft"
+            weight:{
+                value: 0.2001,
+                unit: "lb/ft"
+            }
         },
         {
             id: "0-140-05-ALBWH",
             name: "XL Aluminum Bottom Rail White 19'",
-            weight: 0.7586,
-            weightUnit: "lb/ft"
+            weight:{
+                value: 0.7586,
+                unit: "lb/ft"
+            }
         },
         {
             id: "0-160-CA-00A19",
             name: "Roman-1\" Hembar Satin Anodized 19'",
-            weight: 0.1464,
-            weightUnit: "lb/ft"
+            weight:{
+                value: 0.1464,
+                unit: "lb/ft"
+            }
         }
     ]
-
-    convertToGPerMM(weight: number, weightUnit: string): number {
-        switch (weightUnit) {
-            case "lb/ft":
-                return weight * 0.000478802588996763;
-            case "kg/m":
-                return weight;case "g/m":
-            default:
-                console.error("Invalid unit of measure, Bottom Rail Weight " + weightUnit);
-                return 0;
-        }
-    }
 
     addBottomRail(bottomRail: BottomRail): void {
         if(this.getBottomRail(bottomRail.id) === undefined){
             //check unit of measure
-            if(bottomRail.weightUnit === "lb/ft"){
+            if(bottomRail.weight.unit === "lb/ft"){
                 this.bottomRails.push(bottomRail);
-            }else if (bottomRail.weightUnit === "kg/m") {
-                bottomRail.weight = bottomRail.weight * 0.67196897;
-                bottomRail.weightUnit = "lb/ft";
+            }else if (bottomRail.weight.unit === "kg/m") {
+                bottomRail.weight.value = bottomRail.weight.value * 0.67196897;
+                bottomRail.weight.unit = "lb/ft";
                 this.bottomRails.push(bottomRail);
             } else {
                 console.error("Invalid unit of measure");
             }
         }
+    }
+
+    private constructor() {}
+
+    public static getInstance(): BottomRailService {
+        if (!BottomRailService.instance) {
+            BottomRailService.instance = new BottomRailService();
+        }
+        return BottomRailService.instance;
     }
 
     editBottomRail(bottomRail: BottomRail): void {
